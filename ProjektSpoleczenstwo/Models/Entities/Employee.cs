86f03547de2c01ebaf2eb8846c1ac3205c12ba91
Kamil Strzępek <kamil.strzepek@kneiti.prz.edu.pl>
2019-05-21 19:39:25 +0200

    Basic Entities

---
+++ ProjektSpoleczenstwo/Models/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektSpoleczenstwo.Models.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Age { get; set; }
+        public string Token { get; set; }
+        public ICollection<WorkHours> WorkHours { get; set; }
+    }
+}
---
+++ ProjektSpoleczenstwo/Models/Entities/WorkHours.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektSpoleczenstwo.Models.Entities
+{
+    public class WorkHours
+    {
+        public int Id { get; set; }
+        public DateTime PunchTime { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
